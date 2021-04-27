6de71829cc3450494062198b34b5fbf37b3ba178
Caypoh23 <caypoh23@gmail.com>
2021-04-27 14:15:37 +0500

    feat: hand job

---
+++ Assets/Scripts/HandGenerator.cs
@@ -0,0 +1,30 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+
+public class HandGenerator : MonoBehaviour
+{
+    [SerializeField] private HandStruct[] hands;
+    [SerializeField] private float duration = 2;
+    private int _minIndex = 0;
+    private int _maxIndex;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var hand in hands)
+            {
+                hand.hand.transform.DOMove(hand.target.position, duration)
+                    .SetEase(Ease.OutCubic)
+                    .OnComplete(() =>
+                    {
+                        hand.hand.transform.DOMove(hand.initialPoisition.position, duration);
+                    });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HandStruct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[Serializable]
+public struct HandStruct
+{
+    public GameObject hand;
+    public Transform initialPoisition;
+    public Transform target;
+}
