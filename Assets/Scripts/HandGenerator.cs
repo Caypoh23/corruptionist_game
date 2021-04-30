02f3ceecc4591faeb207f3ada9ac8571ac1f24de
00007417a <irgashevb23@gmail.com>
2021-04-30 11:53:23 +0500

    fix: hand work fixed. timer added

--- Assets/Scripts/HandGenerator.cs
+++ Assets/Scripts/HandGenerator.cs
@@ -9,18 +9,22 @@ using Random = UnityEngine.Random;
 public class HandGenerator : MonoBehaviour
 {
     [SerializeField] private HandStruct[] hands;
-    [SerializeField] private float duration = 2;
-    private float speed = 1;
-    private int _index;
+    [SerializeField] private float movementTime = 2;
+    [SerializeField] private float stayDuration = 1;
 
-    private void Awake()
-    {
-    }
+    public bool CanPlay { get; set; } = true;
+
+    private int _index;
 
     private void Update()
     {
-        InvokeRepeating($"MoveHandLerp", 0f, 0.2f);
-        hands[_index].cashGO.SetActive(true);
+        //InvokeRepeating($"MoveHandLerp", 0f, 0.2f);
+        if (CanPlay)
+        {
+            var randomIndex = Random.Range(0, hands.Length);
+            StartCoroutine(MoveObject(randomIndex));
+            hands[randomIndex].cashGO.SetActive(true);
+        }
     }
 
     /*private void MoveHand()
@@ -34,11 +38,27 @@ public class HandGenerator : MonoBehaviour
             });
     }*/
 
-    private void MoveHandLerp()
+    /*private void MoveHandLerp()
     {
         _index = Random.Range(0, hands.Length);
         hands[_index].handGO.transform.position = Vector3.Lerp(hands[_index].initialPosition.position,
             hands[_index].target.position,
             Mathf.PingPong(Time.time / duration, 2));
+    }*/
+
+    private IEnumerator MoveObject(int index)
+    {
+        hands[index].handGO.transform.DOMove(hands[index].target.position, movementTime)
+            .SetEase(Ease.OutCubic);
+        
+        CanPlay = false;
+        yield return new WaitForSeconds(stayDuration + movementTime);
+        
+        if (!CanPlay)
+        {
+            hands[index].handGO.transform.DOMove(hands[index].initialPosition.position, movementTime);
+            yield return new WaitForSeconds(movementTime);
+            CanPlay = true;
+        }
     }
 }
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] private int currentLevel = 1;
+    [SerializeField] private float maxTimerValue;
+    [SerializeField] private HandGenerator handGenerator;
+    [SerializeField] private int loopNumber;
+    
+    private float _currentTimerValue;
+
+    private void Awake()
+    {
+        _currentTimerValue = maxTimerValue;
+    }
+
+    private void Update()
+    {
+        LevelTimer();
+    }
+    
+    // Rename method name
+    private void LevelTimer()
+    {
+        if (_currentTimerValue > 0 && loopNumber != 0)
+        {
+            _currentTimerValue -= Time.deltaTime;
+        }
+        else if (_currentTimerValue <= 0)
+        {
+            currentLevel++;
+            loopNumber--;
+            _currentTimerValue = maxTimerValue;
+            Debug.Log("Game over or start next level. Current level: " + currentLevel + " " + handGenerator.CanPlay);
+        }
+        // Think of better method
+        else if(loopNumber == 0)
+        {
+            handGenerator.CanPlay = false;
+        }
+    }
+    
+    
+}
